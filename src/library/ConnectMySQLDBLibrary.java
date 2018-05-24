b1382cdc4add9a2cf8b3d94c2c2480de3b53cc07
thanhtuyenit <nguyenthithanhtuyen.dr@gmail.com>
2018-05-24 14:23:56 +0700

    update connect database

--- src/library/ConnectMySQLDBLibrary.java
+++ src/library/ConnectMySQLDBLibrary.java
@@ -3,32 +3,31 @@ package library;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectMySQLDBLibrary {
-	
+
 	private Connection connection;
 	private String url;
 	private String user;
 	private String pass;
-	private ReadPropertiesLibrary propertiesLibrary;
-	private Properties properties = null;
-	public ConnectMySQLDBLibrary(){
-		propertiesLibrary = new ReadPropertiesLibrary();
-		properties = propertiesLibrary.readProperties();
-		this.url = properties.getProperty("url");
-		this.user = properties.getProperty("username");
-		this.pass = properties.getProperty("password");
+
+	public ConnectMySQLDBLibrary() {
+		this.url = "jdbc:mysql://localhost:3306/LTM?useUnicode=true&characterEncoding=UTF-8";
+		this.user = "root";
+		this.pass = "1996Tuyen";
 	}
-	public Connection getConnectMySQL(){
+
+	public Connection getConnectMySQL() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(url, user, pass);
 		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
+			System.out.println("Lỗi kết nối!");
 		}
+
 		return connection;
 	}
+
 	public static void main(String[] args) {
 		ConnectMySQLDBLibrary conn = new ConnectMySQLDBLibrary();
 		System.out.println(conn.getConnectMySQL());
